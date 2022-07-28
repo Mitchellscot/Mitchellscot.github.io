c9602313ce6aa5eab5c4c861f7818106487a044a
Mitchell <mitchellscott@me.com>
2022-07-28 16:37:39 -0500

    added stuff for blog (home) page

---
+++ frontend/src/pages/blog.tsx
@@ -0,0 +1,23 @@
+import {GetStaticProps, InferGetStaticPropsType} from 'next';
+import {NextSeo} from 'next-seo';
+import HomePageData from '../models/HomePageData';
+import styles from '../styles/pages/Index.module.scss';
+import pageTitle from '../utils/pageTitle';
+import {getHomePage} from '../utils/static-props';
+
+export default function Blog(
+  homePageData: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  return (
+    <>
+      <NextSeo
+        title={pageTitle(homePageData.pageTitle)}
+        description={homePageData.metaDescription}
+      />
+      <div className={styles.container}>Hello Mitchell!</div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps<HomePageData> = async (context) =>
+  getHomePage(context);
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,25 +1,7 @@
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
-import { NextSeo } from 'next-seo';
-import HomePageData from '../models/HomePageData';
-import styles from '../styles/pages/Index.module.scss';
-import pageTitle from '../utils/pageTitle';
-import {getHomePage} from '../utils/static-props';
-
-export default function Home(
-  homePageData: InferGetStaticPropsType<typeof getStaticProps>){
+export default function Index() {
   return (
     <>
-      <NextSeo
-        title={pageTitle(homePageData.pageTitle)}
-        description={homePageData.metaDescription}
-      />
-      <div className={styles.container}>
-        Hello world!
-      </div>
+      <div>Hello world!</div>
     </>
-
-  )
+  );
 }
-
-export const getStaticProps: GetStaticProps<HomePageData> = async (context) =>
-  getHomePage(context);
