1c3b35203780f965948b12875b571f11f750a1cd
Mitchell <mitchellscott@me.com>
2022-07-29 13:43:41 -0500

    footer tweak

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -11,23 +11,22 @@ export default function Footer() {
       <div className={styles.socialLinks}>
         <Link href="https://www.strava.com/athletes/mitchellscot" passHref>
           <a target="_blank">
-            <Image src="/strava.svg" height="50" width="50" alt="" />
+            <Image src="/strava.svg" height="40" width="40" alt="" />
           </a>
         </Link>
-
         <Link href="https://twitter.com/_mitchellscot" passHref>
           <a target="_blank">
-            <Image src="/twitter.svg" height="50" width="50" alt="" />
+            <Image src="/twitter.svg" height="40" width="40" alt="" />
           </a>
         </Link>
         <Link href="https://www.linkedin.com/in/mitchellscot" passHref>
           <a target="_blank">
-            <Image src="/linkedIn.svg" height="50" width="50" alt="" />
+            <Image src="/linkedIn.svg" height="40" width="40" alt="" />
           </a>
         </Link>
         <Link href="https://github.com/Mitchellscot" passHref>
           <a target="_blank">
-            <Image src="/github.svg" height="50" width="50" alt="" />
+            <Image src="/github.svg" height="40" width="40" alt="" />
           </a>
         </Link>
       </div>
