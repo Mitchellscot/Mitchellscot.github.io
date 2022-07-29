f713e2976a00f96e0abccf61c73cb33ca893033e
Mitchell <mitchellscott@me.com>
2022-07-29 13:26:53 -0500

    added footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,3 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Footer.module.scss';
+
 export default function Footer() {
-    return(<></>);
+  return (
+    <footer className={styles.container}>
+      <p className={styles.copyright}>
+        &copy;{' 2022 Mitchell Scott. All rights reserved.'}
+      </p>
+      <div className={styles.socialLinks}>
+        <Link href="https://www.strava.com/athletes/mitchellscot" passHref>
+          <a target="_blank">
+            <Image src="/strava.svg" height="50" width="50" alt="" />
+          </a>
+        </Link>
+
+        <Link href="https://twitter.com/_mitchellscot" passHref>
+          <a target="_blank">
+            <Image src="/twitter.svg" height="50" width="50" alt="" />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/mitchellscot" passHref>
+          <a target="_blank">
+            <Image src="/linkedIn.svg" height="50" width="50" alt="" />
+          </a>
+        </Link>
+        <Link href="https://github.com/Mitchellscot" passHref>
+          <a target="_blank">
+            <Image src="/github.svg" height="50" width="50" alt="" />
+          </a>
+        </Link>
+      </div>
+    </footer>
+  );
 }
