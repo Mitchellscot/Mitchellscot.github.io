fac276cc9a1967aa6c52c33bcfbf23e968fbd1eb
Mitchell Scott <mitchellscott@me.com>
2024-12-28 13:35:14 -0600

    add page title

--- frontend/src/app/stats/layout.tsx
+++ frontend/src/app/stats/layout.tsx
@@ -1,4 +1,10 @@
 import Layout from '../../components/Layout/Layout';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+  title: 'Workout Statistics',
+  description: "Mitchell Scott's Workout Statistics",
+};
 
 export default function StatsLayout({children}: {children: React.ReactNode}) {
   return <Layout path={'stats'}>{children}</Layout>;
